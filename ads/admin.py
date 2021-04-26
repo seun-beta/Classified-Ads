56e55308f4459e402a6d32cd635575e55e75f87e
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-04-26 14:47:54 +0100

    add model connection to admin

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from ads.models import Ad, Comment
 
-# Register your models here.
+class AdAdmin(admin.ModelAdmin):
+    exclude = ('picture', 'content_type')
+
+admin.site.register(Comment, AdAdmin)
